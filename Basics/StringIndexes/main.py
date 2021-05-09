86ef0bb02035cc901ef84009d5ce4e498496e76c
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-09 11:11:58 -0500

    String Indexes

---
+++ Basics/StringIndexes/main.py
@@ -0,0 +1,16 @@
+selfish = 'me me me'
+         # 01234567
+print(selfish[1]) # e
+
+# [start:stop]
+print(selfish[0:6]) # me me
+
+# [start:stop:stepover]
+nums = '01234567'
+print(nums[0:8:2]) # 0246
+print(nums[1:]) # 1234567
+print(nums[:5]) # 01234
+print(nums[::1]) # 01234567
+print(nums[-1]) # 7
+print(nums[::-1]) # 76543210
+print(nums[::-2]) # 7531
