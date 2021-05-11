69eee06a176d5466fcfe5bb715c6a4d4f0d6fc05
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-11 15:32:41 -0500

    Built-in methods

---
+++ Basics/BuiltInMethods/main.py
@@ -0,0 +1,6 @@
+quote = 'to be or not to be'
+print(quote.upper()) # TO BE OR NOT TO BE 
+print(quote.capitalize()) # To be or not to be
+print(quote.replace('be', 'me')) # to me or not to me
+print(quote) # still 'to be or not to be' the methods don't replace
+# the value
