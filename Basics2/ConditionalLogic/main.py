a813a725ade70dc0cca3f5c48c891e28db301ef3
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-20 19:32:46 -0500

    Conditional logic

---
+++ Basics2/ConditionalLogic/main.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licensed = True
+
+if is_old and is_licensed:
+    print('You are old enough to drive, and you have a license!')
+# elif is_licensed:
+#     print('You can drive now!')
+else:
+    print('You are too young to drive!')
