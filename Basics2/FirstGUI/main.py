c3c6b6ae6744136c290c90d603f008dfbb01ac70
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-26 17:58:32 -0500

    Cleaning up code

--- Basics2/FirstGUI/main.py
+++ Basics2/FirstGUI/main.py
@@ -11,12 +11,14 @@ picture = [
 # iterate over picture
 # if 0 -> print ' '
 # if 1 -> print *
+fill = '*'
+empty = ' '
 for row in picture:
     for pixel in row:
-        if (pixel == 1):
-            print('*', end='')
+        if pixel:
+            print(fill, end='')
         else:
-            print(' ', end='')
+            print(empty, end='')
     print('')
 
 #        *
