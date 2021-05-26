25e95af226a9e90b72619012a299ac7088d48d69
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-26 18:25:53 -0500

    Function basics

---
+++ Basics2/Functions/main.py
@@ -0,0 +1,37 @@
+def say_hello():
+    print('Hello')
+
+
+say_hello()
+# Hello
+
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+
+def show_tree():
+    fill = '*'
+    empty = ' '
+    for row in picture:
+        for pixel in row:
+            if pixel:
+                print(fill, end='')
+            else:
+                print(empty, end='')
+    print('')
+
+
+show_tree()
+
+#        *
+#       ***
+#      *****
+#     *******
+#        *
+#        *
