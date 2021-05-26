f13ef1074b92359dfc3efd0bd4e5e2041ffdc2a3
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-26 17:49:27 -0500

    First GUI

---
+++ Basics2/FirstGUI/main.py
@@ -0,0 +1,27 @@
+# Exercise
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+# iterate over picture
+# if 0 -> print ' '
+# if 1 -> print *
+for row in picture:
+    for pixel in row:
+        if (pixel == 1):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('')
+
+#        *
+#       ***
+#      *****
+#     *******
+#        *
+#        *
