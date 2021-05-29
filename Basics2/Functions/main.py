7e262fed7265099000b301460857d200349d692a
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-29 12:48:12 -0500

    return keyword

--- Basics2/Functions/main.py
+++ Basics2/Functions/main.py
@@ -44,3 +44,35 @@ def say_hello(name, emoji):
 
 say_hello('Ryan', ':)')
 # Hello Ryan :)
+
+# keyword arguments
+say_hello(emoji=':)', name='Kayla')
+# Hello Kayla :)
+
+
+# default parameters
+
+
+def say_hello(name='Ryan', emoji=':)'):
+    print('Hello {name} {emoji}')
+
+
+say_hello()
+# Hello Ryan :)
+
+
+# return
+def sum(num1, num2):
+    num1 + num2
+
+
+sum(4, 5)  # nothing shows when we run this
+print(sum(4, 5))  # None
+
+
+# We need the return keyword to return the value
+def sum(num1, num2):
+    return num1 + num2
+
+
+print(sum(4, 5))  # 9
